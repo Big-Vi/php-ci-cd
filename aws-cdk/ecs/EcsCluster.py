b398579c7e81bdbf02831157861c5b8426f1d810
big-vi <vignesh.murugan@hotmail.com>
2022-08-21 08:04:05 +1200

    bringing back cdk into same git repo

---
+++ aws-cdk/ecs/EcsCluster.py
@@ -0,0 +1,104 @@
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_iam as iam,
+    aws_secretsmanager as secretsmanager,
+    Stack, CfnOutput, Fn
+)
+from constructs import Construct
+
+
+class EcsCluster(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        vpc = ec2.Vpc.from_lookup(
+            self, "VPC",
+            vpc_id="vpc-0a2eb88f37dd4d313",
+        )
+
+        vpc_subnets = ec2.SubnetSelection(
+            subnets=[
+                ec2.Subnet.from_subnet_id(
+                    self, "subnet1", "subnet-025da55a2ea069297"),
+                ec2.Subnet.from_subnet_id(
+                    self, "subnet2", "subnet-090a11611b7237db6")
+            ]
+        )
+
+        security_group = ec2.SecurityGroup.from_lookup_by_id(
+            self, "SG", "sg-0cf6eb022d5597677")
+
+        self.cluster = ecs.Cluster(
+            self, 'EcsCluster',
+            vpc=vpc
+        )
+
+        ecsTaskExecutionRole = iam.Role(
+            self, "ecsTaskExecutionRole",
+            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com")
+        )
+        ecsTaskExecutionRole.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name(
+            "service-role/AmazonECSTaskExecutionRolePolicy"))
+
+        self.fargate_task_definition = ecs.FargateTaskDefinition(
+            self, "TaskDef",
+            cpu=256,
+            execution_role=ecsTaskExecutionRole,
+            task_role=ecsTaskExecutionRole
+        )
+        my_secret_from_name = secretsmanager.Secret.from_secret_name_v2(
+            self, "SecretFromName", Fn.import_value("secretName"))
+
+        secrets = {
+            "SS_DATABASE_NAME": ecs.Secret.from_secrets_manager(my_secret_from_name, "dbname"),
+            "SS_DATABASE_PASSWORD": ecs.Secret.from_secrets_manager(my_secret_from_name, "password"),
+            "SS_DATABASE_USERNAME": ecs.Secret.from_secrets_manager(my_secret_from_name, "username"),
+            "SS_DATABASE_SERVER": ecs.Secret.from_secrets_manager(my_secret_from_name, "host"),
+        }
+
+        container = self.fargate_task_definition.add_container(
+            "php",
+            # Use an image from ECR
+            image=ecs.ContainerImage.from_registry(
+                "090426658505.dkr.ecr.ap-southeast-2.amazonaws.com/php:latest"),
+            port_mappings=[ecs.PortMapping(container_port=80)],
+            secrets=secrets,
+            logging=ecs.LogDrivers.aws_logs(stream_prefix="ecs"),
+        )
+
+        self.fargate_service = ecs.FargateService(
+            self, "CDKFargateService",
+            service_name="php",
+            cluster=self.cluster, task_definition=self.fargate_task_definition, 
+            vpc_subnets=vpc_subnets, assign_public_ip=True,
+            security_groups=[security_group]
+        )
+
+        # fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
+        #     self, "CDKFargateService", cluster=self.cluster, task_definition=fargate_task_definition,
+        # )
+        # fargate_service.service.connections.security_groups[0].add_ingress_rule(
+        #     peer=ec2.Peer.ipv4(vpc.vpc_cidr_block),
+        #     connection=ec2.Port.tcp(80),
+        #     description="Allow http inbound from VPC"
+        # )
+
+        self._cluster_name = CfnOutput(
+            self, "ClusterName",
+            value=self.cluster.cluster_name, export_name="ClusterName"
+        )
+        self._service_name = CfnOutput(
+            self, "FargateServiceName",
+            value=self.fargate_service.service_name, export_name="FargateServiceName"
+        )
+        # self._fargate_task_definition = CfnOutput(
+        #     self, "FargateTaskDefinitionName",
+        #     value=self.fargate_task_definition, export_name="FargateTaskDefinitionName"
+        # )
+        # CfnOutput(
+        #     self,
+        #     "CDKFargateLoadBalancerDNS",
+        #     value=fargate_service.load_balancer.load_balancer_dns_name,
+        # )
---
+++ aws-cdk/pipeline.py
@@ -0,0 +1,225 @@
+# from constructs import Construct
+# from aws_cdk import (
+#     Stack, Duration,
+#     aws_codepipeline as codepipeline,
+#     aws_ecs as ecs,
+#     aws_ec2 as ec2,
+#     aws_s3 as s3,
+#     aws_kms as kms,
+#     aws_codepipeline_actions as codepipeline_actions,
+#     aws_codebuild as codebuild,
+#     SecretValue, Duration, Fn, RemovalPolicy, Arn, ArnComponents
+# )
+# import aws_cdk.aws_iam as iam
+
+
+# class Pipeline(Stack):
+
+#     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+#         super().__init__(scope, id, **kwargs)
+
+#         source_output = codepipeline.Artifact()
+
+#         kms_key = kms.Key(
+#             self, "Key",
+#             removal_policy=RemovalPolicy.DESTROY
+#         )
+
+#         bucket = s3.Bucket(
+#             self, 'ArtifactBucket',
+#             bucket_name="php-artifact-bucket",
+#             # encryption=s3.BucketEncryption.KMS,
+#             encryption_key=kms_key,
+#             auto_delete_objects=True,
+#             removal_policy=RemovalPolicy.DESTROY
+#         )
+
+#         source_action = codepipeline_actions.GitHubSourceAction(
+#             action_name="Source",
+#             owner="Big-Vi",
+#             repo="php-ci-cd",
+#             branch="main",
+#             oauth_token=SecretValue.secrets_manager(
+#                 "prod/php-app", json_field='access_token'),
+#             output=codepipeline.Artifact(bucket.bucket_name),
+#         )
+
+#         cb_docker_build = codebuild.PipelineProject(
+#             self, "DockerBuild",
+#             # project_name=f"{props['namespace']}-Docker-Build",
+#             environment=codebuild.BuildEnvironment(
+#                 privileged=True,
+#                 build_image=codebuild.LinuxBuildImage.STANDARD_5_0
+#             ),
+#             description='Pipeline for CodeBuild',
+#             timeout=Duration.minutes(60),
+#         )
+
+#         cb_docker_build.add_to_role_policy(iam.PolicyStatement(
+#             effect=iam.Effect.ALLOW,
+#             actions=[
+#                 "ecr:GetAuthorizationToken",
+#                 "ecr:BatchCheckLayerAvailability",
+#                 "ecr:GetDownloadUrlForLayer",
+#                 "ecr:GetRepositoryPolicy",
+#                 "ecr:DescribeRepositories",
+#                 "ecr:ListImages",
+#                 "ecr:DescribeImages",
+#                 "ecr:BatchGetImage",
+#                 "ecr:GetLifecyclePolicy",
+#                 "ecr:GetLifecyclePolicyPreview",
+#                 "ecr:ListTagsForResource",
+#                 "ecr:DescribeImageScanFindings",
+#                 "ecr:InitiateLayerUpload",
+#                 "ecr:UploadLayerPart",
+#                 "ecr:CompleteLayerUpload",
+#                 "ecr:PutImage"
+#             ],
+#             resources=["*"]
+#         ))
+
+#         vpc = ec2.Vpc.from_lookup(
+#             self, "VPC",
+#             vpc_id="vpc-0a2eb88f37dd4d313",
+#         )
+#         security_group = ec2.SecurityGroup.from_lookup_by_id(
+#             self, "SG", "sg-0cf6eb022d5597677")
+
+#         fargate_service = ecs.FargateService.from_fargate_service_attributes(
+#             self, "Service",
+#             cluster=ecs.Cluster.from_cluster_attributes(
+#                 self, "Cluster",
+#                 cluster_name=Fn.import_value("ClusterName"),
+#                 vpc=vpc,
+#                 security_groups=[security_group]
+#             ),
+#             service_name=Fn.import_value("FargateServiceName")
+#         )
+
+#         pipeline = codepipeline.Pipeline(
+#             self,
+#             "PipelinePHP",
+#             artifact_bucket=bucket,
+#             # pipeline_name=f"{props['namespace']}",
+#             stages=[
+#                 codepipeline.StageProps(
+#                     stage_name="Source", actions=[source_action]),
+#                 codepipeline.StageProps(
+#                     stage_name="Build",
+#                     actions=[
+#                         codepipeline_actions.CodeBuildAction(
+#                             action_name="Build",
+#                             project=cb_docker_build,
+#                             # input=source_output,
+#                             input=codepipeline.Artifact(bucket.bucket_name),
+#                         )
+#                     ]
+#                 ),
+#                 codepipeline.StageProps(
+#                     stage_name="Deploy",
+#                     actions=[
+#                         codepipeline_actions.EcsDeployAction(
+#                             action_name="DeployAction",
+#                             service=fargate_service,
+#                             input=codepipeline.Artifact(bucket.bucket_name),
+#                             deployment_timeout=Duration.minutes(60)
+#                         )
+#                     ]
+#                 ),
+#                 codepipeline.StageProps(
+#                     stage_name="Approve",
+#                     actions=[
+#                         codepipeline_actions.ManualApprovalAction(
+#                             action_name="DeployAction",
+#                         )
+#                     ]
+#                 )
+#             ]
+#         )
+
+#         # approve_stage = pipeline.add_stage(stage_name="Approve")
+
+#         # manual_approval_action = codepipeline_actions.ManualApprovalAction(
+#         #     action_name="Approve"
+#         # )
+#         # approve_stage.add_action(manual_approval_action)
+
+#         # role = iam.Role.from_role_arn(self, "Admin", Arn.format(
+#         #     ArnComponents(service="iam", resource="role", resource_name="Admin"), self))
+#         # manual_approval_action.grant_manual_approval(role)
+
+import json
+import pathlib
+from typing import Any
+
+from aws_cdk import (
+    aws_codebuild as codebuild,
+    pipelines,
+    Stack
+)
+from constructs import Construct
+
+import constants
+
+from deployment import UserManagementBackend
+
+
+class Pipeline(Stack):
+    def __init__(self, scope: Construct, id_: str, **kwargs: Any):
+        super().__init__(scope, id_, **kwargs)
+
+        codepipeline_source = pipelines.CodePipelineSource.connection(
+            f"{constants.GITHUB_OWNER}/{constants.GITHUB_REPO}",
+            constants.GITHUB_TRUNK_BRANCH,
+            connection_arn=constants.GITHUB_CONNECTION_ARN,
+        )
+        synth_python_version = {
+            "phases": {
+                "install": {
+                    "runtime-versions": {"python": constants.CDK_APP_PYTHON_VERSION}
+                }
+            }
+        }
+        synth_codebuild_step = pipelines.CodeBuildStep(
+            "Synth",
+            input=codepipeline_source,
+            partial_build_spec=codebuild.BuildSpec.from_object(
+                synth_python_version),
+            # install_commands=["./scripts/install-deps.sh"],
+            commands=[
+                "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                # Instructs Codebuild to install required packages
+                "pip install -r requirements.txt",
+                "cdk synth",
+            ],
+            primary_output_directory="cdk.out",
+        )
+        codepipeline = pipelines.CodePipeline(
+            self,
+            "CodePipeline",
+            # cli_version=Pipeline._get_cdk_cli_version(),
+            # cross_account_keys=True,
+            synth=synth_codebuild_step,
+        )
+
+        # self._add_prod_stage(codepipeline)
+
+    @staticmethod
+    def _get_cdk_cli_version() -> str:
+        package_json_path = (
+            pathlib.Path(__file__).resolve().parent.joinpath("package.json")
+        )
+        with open(package_json_path) as package_json_file:
+            package_json = json.load(package_json_file)
+        cdk_cli_version = str(package_json["devDependencies"]["aws-cdk"])
+        return cdk_cli_version
+
+    def _add_prod_stage(self, codepipeline: pipelines.CodePipeline) -> None:
+        prod_stage = UserManagementBackend(
+            self,
+            f"{constants.CDK_APP_NAME}-Prod",
+            env=constants.PROD_ENV,
+            instance_type=constants.PROD_DATABASE_INSTANCE_TYPE,
+        )
+        
+        codepipeline.add_stage(prod_stage)
