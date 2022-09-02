ab17b8e6b0193dd1937ae5beadf2dee099d92189
big-vi <vignesh.murugan@hotmail.com>
2022-09-02 13:52:32 +1200

    cron task - task definition

--- aws-cdk/ecs/infrastructure.py
+++ aws-cdk/ecs/infrastructure.py
@@ -77,9 +77,8 @@ class EcsCluster(Construct):
             secrets=secrets,
             logging=ecs.LogDrivers.aws_logs(stream_prefix="ecs"),
             command=[
-                'sh', '-c',
-                'apt-get update && apt-get install iputils-ping && ping -c 3 google.com',
-            ]
+                "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+            ],
         )
 
         self.fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
