1ac6424070a8bbcc85ac0df69f732a0e77c10082
big-vi <vignesh.murugan@hotmail.com>
2022-09-13 09:47:24 +1200

    removing base resources since cdk automatically creates it

--- aws-cdk/Base.py
+++ aws-cdk/Base.py
@@ -1,28 +1,10 @@
 from aws_cdk import (
-    aws_ecr as ecr,
-    aws_secretsmanager as secretsmanager,
-    App, RemovalPolicy, Stack
+    Stack
 )
 from constructs import Construct
 from typing import Any
-
-import constants
-
 
 class Base(Stack):
 
     def __init__(self, scope: Construct, id_: str, **kwargs: Any):
         super().__init__(scope, id_, **kwargs)
-
-        # ecr repo
-        ecr.Repository(
-            self, "ECR",
-            repository_name=constants.CDK_APP_NAME,
-            removal_policy=RemovalPolicy.DESTROY
-        )
-
-        # Secret for git commit hash - needed for CloudFormation dynamically pull it.
-        secretsmanager.Secret(
-            self, "secret-git-commit-hash",
-            secret_name = constants.CDK_APP_NAME + "/git-hash"
-        )
--- aws-cdk/app.py
+++ aws-cdk/app.py
@@ -3,12 +3,8 @@ import aws_cdk as cdk
 
 import constants
 from pipeline import Pipeline
-from Base import Base
 
 app = cdk.App()
-
-# ECR
-Base(app, f"{constants.CDK_APP_NAME}-Base")
 
 # Pipeline
 pipeline = Pipeline(app, f"{constants.CDK_APP_NAME}-Pipeline",
