7069e1736318e9a7c06eabef6caf30e4d0b0de0c
big-vi <vignesh.murugan@hotmail.com>
2022-09-13 15:29:27 +1200

    test

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ REDIS_URL=$1
 
 sed -i.bak "s/session\.save_handler.*/session.save_handler = redis/; s/\;session\.save_path.*/session.save_path = $ESCAPED_REDIS_URL/" /etc/php/7.4/apache2/php.ini
 
-if [ $1 == "True" ]; 
+if [ $2 == "True" ]; 
 
 then
 
